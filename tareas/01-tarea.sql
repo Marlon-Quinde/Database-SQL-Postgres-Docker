c44cefda3af6c46d56c973f4dbda0f940753e01b
Marlon Quinde <71990962+Marlon-Quinde@users.noreply.github.com>
2023-12-25 23:41:37 -0500

    feat(data): Data

--- tareas/01-tarea.sql
+++ tareas/01-tarea.sql
@@ -1,19 +1,24 @@
 
-
 -- 1. Ver todos los registros
-
-
+select * from users
 
 -- 2. Ver el registro cuyo id sea igual a 10
-
+select * from users where users.id =10
 
 -- 3. Quiero todos los registros que cuyo primer nombre sea Jim (engañosa)
+select * from users where LOWER(users.name) Like LOWER('Jim%')
 
 
 -- 4. Todos los registros cuyo segundo nombre es Alexander
+select * from users where users.name like '% Alexander'
 
 
 -- 5. Cambiar el nombre del registro con id = 1, por tu nombre Ej:'Fernando Herrera'
-
+update users set "name" = 'Marlon Quinde' where users.id = 1
+select "name" from users where users.id = 1
 
 -- 6. Borrar el último registro de la tabla
+select * from users where users.id = (select count(*) from users)
+delete from users where users.id = (select count(name) from users)
+delete from users where users.id = (select Max(id) from users)
+select count(*) from users
--- tareas/32-calentamiento.sql
+++ tareas/32-calentamiento.sql
@@ -1,6 +1,74 @@
--- Nombre, apellido e IP, donde la última conexión se dió de 221.XXX.XXX.XXX
+select * from users;
 
+-- Nombre, apellido e IP, donde la última conexión se dió de 221.XXX.XXX.XXX
+select
+	u.first_name as "Nombre",
+	u.last_name as "Apellido",
+	u.last_connection as "Última conexión"
+from
+	users u
+where
+	u.last_connection
+	like '221.%'
 
 
 -- Nombre, apellido y seguidores(followers) de todos a los que lo siguen más de 4600 personas
+	
+select
+	u.first_name as "Nombre",
+	u.last_name as "Apellido",
+	u.followers as "Seguidores"
+from
+	users u
+where
+	--u.followers > 4600 and u.followers < 4700
+	u.followers between 4600 and 4700
+order by
+	u.followers desc
+	
+select
+	count(*) as total_users,
+	min(u.followers) as min_followers,
+	max(u.followers) as max_followers,
+	round(avg(u.followers)) as avg_followers,
+	sum(u.followers) / count(u.followers) as avg_manual 
+from
+	users u
+	
+select
+	count(*),
+	u.followers
+from
+	users u
+	--where u.followers = 4 or u.followers = 4999
+where
+	u.followers between 4500 and 4999
+group by
+	u.followers
+order by
+	u.followers desc
+	
+	
+select
+	count(*),
+	country
+from
+	users u
+group by
+	country
+having
+	count(*) > 5
+order by
+	count(*) desc
+	
+select distinct country from users u
 
+select
+	count(u.email) as cantidad,
+	substring(u.email,(position('@' in u.email) + 1 ) ) as dominio,
+	'Malon' as name
+from
+	users u
+group by substring(u.email,(position('@' in u.email) + 1 ) )
+having count(*) > 1 
+order by substring(u.email,(position('@' in u.email) + 1 ) ) asc
